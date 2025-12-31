15838a9179809448f5a263e96b28439a52158be1
Lockie <202584674+lockieLocks@users.noreply.github.com>
2026-01-01 12:32:47 +1300

    added satus code checker for sites

---
+++ cogs/status_code.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_url_status():
+    url = input("Enter the URL to check its status code: ")
+    try:
+        response = requests.get(url)
+        if response.status_code in [200, 201, 202]:
+            print(f"The URL is reachable. Status Code: {response.status_code}")
+        else:
+            input(f"Status Code >> {response.status_code}\n\nPress Enter to continue...")
+        
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
